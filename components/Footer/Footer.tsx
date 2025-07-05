b0bfacda48ef3d8d7c60d12de53dbdbff0c361f4
ALeksiiHrabovets <hrabovets78@gmail.com>
2025-07-05 16:45:41 +0300

    patch01

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import css from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={css.footer}>
+      <div className={css.content}>
+        <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
+        <div className={css.wrap}>
+          <p>Developer: Aleksii Hrabovets</p>
+          <p>
+            Contact us:
+            <a href="<hrabovets78@gmail.com>"> hrabovets78@gmail.com</a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
